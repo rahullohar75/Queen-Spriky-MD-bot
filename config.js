ac1e97b008dde216d376a33f4300199614ed3bc8
rahullohar75 <rahulohar75@gmail.com>
2024-12-11 20:16:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
+    SESSION_ID: process.env.SESSION_ID || 'OvJlVZ7b#PsLGCRwqpvPj9jikDs9G5IzfBQgZPCT5ZEMwYwFZTfQ', // Enter Your Session ID
     MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '',    // Enter Your Owner Number
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '917017659124',    // Enter Your Owner Number
 };
